ab6716005effd2a9c4e89274f5fa891a6f9e58c8
Borislav Pecherskikh <borislavtarget@gmail.com>
2022-12-02 03:16:47 +0500

    Safety learning

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 
 def index(request):
     template = 'posts/index.html'
     title = 'Yatube для авторов'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title,
-        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
     }
     return render(request, template, context) 
 
